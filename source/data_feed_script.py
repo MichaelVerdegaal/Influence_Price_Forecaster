ab9df32eb3d2f18b690e3f6d1b9da3c0e4598c7e
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2021-12-11 10:55:30 +0100

    Crew count query

--- source/data_feed_script.py
+++ source/data_feed_script.py
@@ -1,10 +1,11 @@
-# Config variables
-from source.queries import multi_asset_retrieval
+from source.queries import multi_asset_retrieval, retrieve_crew_amount
 
+# Config
 collection_slug = 'influence-crew'
 
-# Retrieve a random owner
-x = multi_asset_retrieval(limit=1)
-x2 = multi_asset_retrieval(limit=50)
-x3 = multi_asset_retrieval(limit=50, offset=1)
-print()
+# Get minted crew count, necessary for pagination
+crew_count = retrieve_crew_amount(collection_slug)
+print(f"Retrieving {crew_count} total crew members")
+
+# Placeholder
+random_address = multi_asset_retrieval(slug=collection_slug, limit=1)
--- source/queries.py
+++ source/queries.py
@@ -1,9 +1,7 @@
 import requests
 
-from source.data_feed_script import collection_slug
 
-
-def multi_asset_retrieval(slug: str = collection_slug, limit: int = None, offset: int = None):
+def multi_asset_retrieval(slug: str, limit: int = None, offset: int = None):
     """
     Retrieves multiple assets from OpenSea
     :param slug: slug of the collection to retrieve from
@@ -11,11 +9,18 @@ def multi_asset_retrieval(slug: str = collection_slug, limit: int = None, offset
     :param offset: number to offset pagination with
     :return: list of dictionaries with assets
     """
-    multi_asset_request_url = f"https://api.opensea.io/api/v1/assets?collection={slug}"
+    multi_asset_request_url = f'https://api.opensea.io/api/v1/assets?collection={slug}'
     if limit and (1 <= limit <= 50):
         multi_asset_request_url += f'&limit={limit}'
     if offset:
         multi_asset_request_url += f'&offset={offset}'
-    response = requests.request("GET", multi_asset_request_url)
+    response = requests.request('GET', multi_asset_request_url)
     multi_asset_json = response.json()['assets']
     return multi_asset_json
+
+
+def retrieve_crew_amount(slug: str):
+    stat_collection_request_url = f'https://api.opensea.io/api/v1/collection/{slug}/stats'
+    response = requests.request('GET', stat_collection_request_url)
+    item_count = int(response.json()['stats']['count'])
+    return item_count
