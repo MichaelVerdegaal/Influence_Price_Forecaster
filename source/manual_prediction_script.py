8d0a892e30ee844128a31a4ac63090c96b69f157
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2022-01-03 15:04:34 +0100

    Update manual_prediction_script.py
    Bit cleaner for demo purposes

--- source/manual_prediction_script.py
+++ source/manual_prediction_script.py
@@ -2,21 +2,24 @@ from config import read_model
 from config import unpickle_obj
 from dataset_building import get_and_clean_single_asset
 
-# Split dataset
+# Load static files
 dtypes = unpickle_obj('dataset_influence-crew_dtypes')
-
-# Config
-prediction_number = 1530
-traits_to_keep = ['Title', 'Class', 'Collection']  # Which trait types of the NFT to keep
-
-
-# Retrieve single asset
-asset_to_predict = get_and_clean_single_asset(prediction_number, dtypes, to_keep=traits_to_keep,
-                                              year=None, month=None, day=None)
-
-# Split dataset
 model = read_model("model_influence_crew_10686")
 
-# Predict
-prediction = model.predict(asset_to_predict)[0][0]
-print(f"Predicted price for crew choice is: [{prediction}]")
+# Config
+to_predict = [2284, 6028, 6219, 6999, 7348]
+traits_to_keep = ['Title', 'Class', 'Collection']  # Which trait types of the NFT to keep
+
+# Tracking
+predictions = []
+
+for token_id in to_predict:
+    # Prepare input
+    asset_to_predict = get_and_clean_single_asset(token_id, dtypes, to_keep=traits_to_keep,
+                                                  year=None, month=None, day=None)
+
+    # Predict
+    prediction = model.predict(asset_to_predict)[0][0]
+    predictions.append(prediction)
+    print(f"Predicted price for token {token_id} is: {round(prediction, 3)} ETH")
+print(f"\nTotal predicted value of collection: {round(sum(predictions), 3)} ETH")
